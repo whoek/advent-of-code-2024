44efce8c642c2fd811ab1c57453b70e945707404
Willem <willem@matimba.com>
2024-12-01 14:23:23 +0000

    cleanup

--- day1/day1.ml
+++ day1/day1.ml
@@ -1,13 +1,14 @@
 (****************************************
     Day 1 of AOC 2024
     https://adventofcode.com/2024/day/1
+
  *****************************************)
 
 (****** BEGIN helper functions ******)
 
 let first l = List.hd l
 
-let last l = List.hd @@ List.rev l
+let last l = List.rev l |> List.hd
 
 (* read lines a file file *)
 let lines f =
@@ -21,14 +22,13 @@ let occur v l =
 
 (****** END helper functions ******)
 
-
 let file = "input.txt"
 
-(* get data and filter out empty lines *)
+(* get data and filter out blank strings -- last one was empty *)
 let data =  lines file
             |> List.filter (fun x -> (String.length x) > 0)
 
-(* get left & right value in line  -> convert to INT -> sort list *)
+(* get left & right value in every string -> convert to INT -> sort list *)
 
 let ll = List.map
     (fun x -> int_of_string @@ first @@ String.split_on_char ' ' x) data
@@ -41,9 +41,9 @@ let rl = List.map
 let diff = List.map2 (fun x y -> abs(x - y)) ll rl
 
 let () =
-  Printf.printf "Part 1 Sum:  %i\n" (List.fold_left (+) 0 diff)
+  Printf.printf "Part 1 - sum is:  %i\n" (List.fold_left (+) 0 diff)
 
 let simm = List.map (fun x -> x * occur x rl) ll
 
 let () =
-  Printf.printf "Part 2 Sum:  %i\n" (List.fold_left (+) 0 simm)
+  Printf.printf "Part 2 - sum is:  %i\n" (List.fold_left (+) 0 simm)
