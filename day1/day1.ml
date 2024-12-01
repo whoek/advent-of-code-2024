a00e715adf82bd38339a5b9a00fe351612756c4c
Willem <willem@matimba.com>
2024-12-01 14:51:51 +0000

    cleanup

--- day1/day1.ml
+++ day1/day1.ml
@@ -4,13 +4,13 @@ let first l = List.hd l
 
 let last l = List.rev l |> List.hd
 
-(* read lines a file file *)
+(* read file into a list - every line is a element *)
 let lines f =
   let contents = In_channel.with_open_bin f
       In_channel.input_all in
   String.split_on_char '\n' contents
 
-(* how many time does element v occur in list l *)
+(* how many times does element v occur in list l *)
 let occur v l =
   List.find_all (fun x -> x = v) l |> List.length
 
@@ -18,18 +18,18 @@ let occur v l =
 
 let file = "input.txt"
 
-(* get data and filter out blank strings -- last one was empty *)
+(* get data and filter out empty lines -- last one was empty *)
 let data =  lines file
             |> List.filter (fun x -> (String.length x) > 0)
 
 (* get left & right value in every string -> convert to INT -> sort list *)
 
 let ll = List.map
-    (fun x -> int_of_string @@ first @@ String.split_on_char ' ' x) data
+    (fun x -> String.split_on_char ' ' x |> first |> int_of_string) data
          |>  List.sort compare
 
 let rl = List.map
-    (fun x -> int_of_string @@ last @@ String.split_on_char ' ' x) data
+    (fun x -> String.split_on_char ' ' x |> last |> int_of_string) data
          |>  List.sort compare
 
 let diff = List.map2 (fun x y -> abs(x - y)) ll rl
