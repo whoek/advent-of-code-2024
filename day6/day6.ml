0e30093de7a89deb44bf0105c6d7b613070748fc
Willem <willem@matimba.com>
2024-12-08 21:23:13 +0000

    day6 part 1

--- day6/day6.ml
+++ day6/day6.ml
@@ -7,60 +7,67 @@ let load file =
       In_channel.input_all in
   String.split_on_char '\n' contents
 
+let find h k = Hashtbl.find h k
+let replace h k v  = Hashtbl.replace h k v
+
 (* get data and filter out empty lines *)
 let data =  load input_file
             |> List.filter (fun x -> (String.length x) > 0)
 
-(* data : string list *)
+(* load in ht : (x:int * y:int, char) Hashtbl *)
 let ht = Hashtbl.create 20000
 
-(* load in Hashtable *)
 let () = List.iteri (fun iy y ->
-    String.iteri (fun ix x -> Hashtbl.replace ht (ix, iy) x
+    String.iteri (fun ix x -> replace ht (ix, iy) x
                  ) y ) data
 
-let Some ((start_x,start_y), start_direction)   =
+(* find start *)
+let (start_x, start_y), start_point =
   Hashtbl.fold (fun k v accu ->
       match v with
-      | '^' | '<' | '>' | 'v'->  Some (k, v)
+      | '^' | '<' | '>' | 'v'->  (k, v)
       | _ -> accu
-    ) ht None
+    ) ht ((0,0),' ')
 
-let x = ref start_x
-let y = ref start_y
-let direction = ref start_direction
-
-
-(*
 let () =
-  match (!direction) with
-  | '^' -> begin
-      if ABAOVE != 'hash' then  make current X and move curser up
-else if ABOVE = # then go right
- x< new
-y new
-    end
-  | '<' ->  begin  end
-  | '>' -> begin  end
-  | 'v'->  begin  end
-  | _ -> failwith "Invalid state"
+  let point = ref start_point in
+  let x = ref start_x in
+  let y = ref start_y in
+  let step = ref 0 in
+  try
+    while true do
+      step := !step + 1;
+      point := find ht (!x, !y);
+      Printf.printf "step: %i - (x: %i y: %i)   %c\n%!" !step !x !y !point;
+      replace ht (!x,!y)  'X';
+      match !point with
+      | '^' ->
+        if find ht (!x,!y - 1) = '#'
+        then (replace ht (!x + 1,!y) '>'; x := !x + 1;)
+        else (replace ht (!x,!y - 1) '^'; y := !y - 1;)
+      | 'v'->
+        if find ht (!x,!y + 1) = '#'
+        then (replace ht (!x - 1,!y) '<'; x := !x - 1;)
+        else (replace ht (!x,!y + 1) 'v'; y := !y + 1;)
+      | '<' ->
+        if find ht (!x - 1, !y) = '#'
+        then (replace ht (!x, !y - 1) '^'; y := !y - 1;)
+        else (replace ht (!x - 1, !y) '<'; x := !x - 1;)
+      | '>' ->
+        if find ht (!x + 1, !y) = '#'
+        then (replace ht (!x, !y + 1) 'v'; y := !y + 1; )
+        else (replace ht (!x + 1,!y)  '>'; x := !x + 1; )
+      | _ -> failwith "Invalid state"
+    done
+  with _ -> ()
 
-*)
+let p1 =
+  Hashtbl.fold (fun k v accu ->
+      match v with
+      | 'X' -> accu + 1
+      | _ -> accu
+    ) ht 0
 
-(*
-let () = Hashtbl.add ht 1 "one "
-let () = Hashtbl.replace ht 2 "two"    (* remove + add *)
+let () = Printf.printf "part 1 = %i\n" p1
 
-let a = Hashtbl.find ht 1
-let b = Hashtbl.find ht 2
-
-*)
-
-
-
-
-
-
-(* find starting point *)
-
-(* check for obst -- then advance *)
+(* part 1 -- 4977  *)
