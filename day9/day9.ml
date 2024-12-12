c57db4350cb4ed7a3f3f3805f44dbc82affdcd70
Willem <willem@matimba.com>
2024-12-12 17:31:22 +0000

    day9 changes - in progess

--- day9/day9.ml
+++ day9/day9.ml
@@ -4,52 +4,55 @@ let input_file = "input.txt"
 
 let load file =
   let contents = In_channel.with_open_bin file
-      In_channel.input_all in
-  String.split_on_char '\n' contents
+      In_channel.input_all
+  in
+  (* String.split_on_char '\n' contents *)
+  contents
 
-let data =  load input_file
-            |> List.filter (fun x -> (String.length x) > 0)
-            (* |> List.map (fun x -> String.split_on_char '\n' x) *)
+let explode s = List.init (String.length s) (String.get s)
 
+let () = assert (explode "123" = ['1'; '2'; '3'])
 
-let find h k = Hashtbl.find h k
-let replace h k v  = Hashtbl.replace h k v
+let rec pair l =
+  match l with
+  | v1 :: v2 :: rest -> (v1, v2) :: pair rest
+  | v1 :: [] -> [(v1, 0)]
+  | [] -> []
 
-let input = Hashtbl.create 20000
-
-let () = List.hd data
-         |> String.iteri (fun i x ->  replace input i @@ (int_of_char x) - 48 )
-
-let output = Hashtbl.create 100_000
-
-
+let () = assert (pair [1; 2; 3; 4] =  [(1, 2); (3, 4)])
+let () = assert (pair [1; 2; 3; 4; 5] =  [(1, 2); (3, 4); (5, 0)])
 
 
+let dense =  load input_file
+                |> String.trim
+                |> explode
+                |> List.map (fun x -> int_of_char x - 48)
+                |> pair
+                |> List.mapi (fun i x -> i mod 10, x)   (* allocate ID *)
 
 
-(* NOTES -- delete when done
+let id_files (id_no, (file_count, space_count)) =
+  List.init file_count (fun _ -> Some id_no  )
+  @   List.init space_count (fun _ -> None )
 
-12345
+let expand_disk = dense
+                  |> List.map id_files
+                  |> List.concat
 
-0..111....22222
+let to_move_qty = List.filter (fun x -> x <> None) expand_disk
 
-
-file_length
-free_length
-
-number of digits = 19_999
-
-
-
-start with id 0
-file / space / file / space etc
-
-move fields -- 1-by-1 to LEFT
-
-calc checksum
-SUM OF (value x position )
+let to_move = expand_disk
+  |> List.filter (fun x -> x <> None)
+  |> List.rev
 
 
-54707 .... 61433
+(*
+
+expand disk
+   44955 with '.'
+   49767 with a number
+ = 94722 TOTAL
+
+All the numbers must be in first 49767 elements
 
 *)
